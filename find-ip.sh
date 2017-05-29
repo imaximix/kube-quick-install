0fee4af51e026b186033564a4d6246b9adce6ffc
imaximix <maximilian1991@gmail.com>
2017-05-29 16:43:07 +0300

    Update find-ip.sh

--- find-ip.sh
+++ find-ip.sh
@@ -1,2 +1,4 @@
 public_ip="$(ifconfig eth0 | grep 'inet ' | awk -F' ' '{print $2}')"
 sed 's/$private_ip/'"$public_ip"'/g'
+
+curl -s https://raw.githubusercontent.com/imaximix/kube-quick-install/master/node | sed 's/$public_ip/'"$(ifconfig eth0 | grep 'inet ' | awk -F' ' '{print $2}')"'/g'
